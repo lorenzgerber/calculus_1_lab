ad2840f1361b22356b0992232a6db435154ec6a9
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-11-18 17:57:27 +0100

    playing around with functions

---
+++ calculus.R
@@ -0,0 +1,13 @@
+f = function (x) 3*x^4+4*x-5
+x = seq(-1.4,1.4,0.1)
+plot(f(x))
+
+nderiv = function (f, x, h) (f(x+h)-f(x))/h
+f = function (x) cos(x)
+nderiv(f,0.5, 0.0001)
+df = function (x) nderiv(f, x, 0.0001)
+plot(x, f(x), type='b')
+points(x, df(x), type='b')
+
+f = function (x) x^11 - 5*x^2+exp(x)
+
