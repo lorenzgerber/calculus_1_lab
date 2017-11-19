2864c7e86810b47fdf691609475663453bd8d448
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-11-19 13:51:13 +0100

    started with report

--- calculus.R
+++ calculus.R
@@ -1,5 +1,5 @@
 f = function (x) 3*x^4+4*x-5
-x = seq(-1.4,1.4,0.1)
+x = seq(-2,2,0.1)
 plot(f(x))
 
 nderiv = function (f, x, h) (f(x+h)-f(x))/h
@@ -10,4 +10,13 @@ plot(x, f(x), type='b')
 points(x, df(x), type='b')
 
 f = function (x) x^11 - 5*x^2+exp(x)
+
+newton = function (f, x, n) {
+  for(i in 1:n){
+    x <- x-f(x)/nderiv(f,x,0.0001)
+  }
+  return(x)
+}
+
+
 
